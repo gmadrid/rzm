e4ba155602829b956b415013daf98c780ee7ae28
George Madrid <gmadrid@gmail.com>
2016-10-29 10:46:43 -0400

    adding two-byte offset test for jumps.

--- src/zmachine/ops/twoops/je_0x01.rs
+++ src/zmachine/ops/twoops/je_0x01.rs
@@ -63,16 +63,21 @@ mod test {
             Operands::Two(Operand::SmallConstant(4), Operand::SmallConstant(4)))
       .unwrap();
     assert_eq!(400, runner.current_pc());
+
+    let mut vec = vec![0u8; 500];
+    runner.set_jump_offset_word(-400, true);
+    vec.append(&mut runner.pcbytes);
+    runner.set_pc_bytes(vec);
+    runner.pc = 500;
+    je_0x01(&mut runner,
+            Operands::Two(Operand::SmallConstant(6), Operand::SmallConstant(6)))
+      .unwrap();
+    assert_eq!(100, runner.current_pc());
   }
 
   #[test]
   fn test_je() {
     let mut runner = TestRunner::new();
-
-    // TODO: write these tests
-    // test one one-byte
-    // test two bytes
-    // test two bytes negative
 
     // test ret false
     // test ret true
