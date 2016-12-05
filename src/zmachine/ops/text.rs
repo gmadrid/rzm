7c58a55ca4b3a08e29eebfdc026272c2d3ea6548
George Madrid <gmadrid@gmail.com>
2016-12-05 16:22:37 -0500

    Whoops. Looks like that wasn't broken.

--- src/zmachine/ops/text.rs
+++ src/zmachine/ops/text.rs
@@ -95,6 +95,7 @@ fn decode_text<T>(vm: &mut T, src: TextSource) -> Result<String>
           }
           State::SecondHalfZChar => {
             let zchar = (first_half + *ch) as u8 as char;
+            print!("{}", zchar);
             state = State::Normal;
           }
         }
